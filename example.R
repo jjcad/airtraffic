8e9a065793af6fbaf6a9e915f1bb8f033aa4ef36
jeremy jackson <jeremy.jackson@cadmusgroup.com>
2017-02-16 05:39:17 +0000

    Updated to use multiple files.

--- example.R
+++ example.R
@@ -3,4 +3,4 @@ library(dplyr)
 
 sc <- spark_connect(master = 'local')
 
-air2008 <- spark_read_csv(sc, 'air2008', '2008.csv', null_value = NA)
+air <- spark_read_csv(sc, 'air', '*.csv', null_value = NA)
---
+++ getAirtrafficData.R
@@ -0,0 +1,8 @@
+# Grab the data. Can take a few minutes.
+download.file('http://stat-computing.org/dataexpo/2009/2008.csv.bz2', '2008.csv.bz2')
+download.file('http://stat-computing.org/dataexpo/2009/2007.csv.bz2', '2007.csv.bz2')
+download.file('http://stat-computing.org/dataexpo/2009/2006.csv.bz2', '2006.csv.bz2')
+download.file('http://stat-computing.org/dataexpo/2009/2005.csv.bz2', '2005.csv.bz2')
+
+# Unzipping them all might take a minute...
+system2('bunzip2', '*.bz2')
